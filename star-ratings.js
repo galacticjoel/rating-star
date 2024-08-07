7a7e49d2bd7ebd6b2d0f4575410163a7ee697e36
galacticjoel <Joel1980@yandex.com>
2024-08-07 18:18:25 +0200

    minor correction

--- star-ratings.js
+++ star-ratings.js
@@ -4,10 +4,11 @@ let showValue = document.querySelector('#rating-value');
 for (let i = 0; i < star.length; i++) {
 	star[i].addEventListener('click', function() {
 		i = this.value;
-
+		const element = document.getElementById("hidden");
+		element.remove();
 		showValue.innerHTML = i + " out of 5";
 	});
 }
 
 
-  
+
